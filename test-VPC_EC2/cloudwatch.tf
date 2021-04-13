c2331f1c99d58af9fda8d9fd5dabad2a4596a5a2
ArerepadeBenagha <80588773+ArerepadeBenagha@users.noreply.github.com>
2021-04-12 23:48:21 -0500

    Create cloudwatch.tf

---
+++ test-VPC_EC2/cloudwatch.tf
@@ -0,0 +1,84 @@
+resource "aws_cloudwatch_metric_alarm" "main" {
+  alarm_name                = "terraform-test-main"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
+resource "aws_autoscaling_policy" "bat" {
+  name                   = "main-terraform-test"
+  scaling_adjustment     = 4
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.main-1.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "main-1" {
+  alarm_name          = "terraform-test-main-1"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.main-1.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.main-1.arn]
+}
+resource "aws_cloudwatch_metric_alarm" "main-2" {
+  alarm_name                = "terraform-test-main"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  threshold                 = "10"
+  alarm_description         = "Request error rate has exceeded 10%"
+  insufficient_data_actions = []
+
+  metric_query {
+    id          = "e1"
+    expression  = "m2/m1*100"
+    label       = "Error Rate"
+    return_data = "true"
+  }
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "RequestCount"
+      namespace   = "AWS/ApplicationELB"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        LoadBalancer = "app/web"
+      }
+    }
+  }
+
+  metric_query {
+    id = "m2"
+
+    metric {
+      metric_name = "HTTPCode_ELB_50_count"
+      namespace   = "AWS/ApplicationELB"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        LoadBalancer = "app/web"
+      }
+    }
+  }
+}
