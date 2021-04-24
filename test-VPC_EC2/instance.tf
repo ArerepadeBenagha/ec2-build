06f8815ea47e13ebc8d4272e7d1e13e8b99c41ae
ArerepadeB <aredelulu@gmail.com>
2021-04-24 17:16:39 -0500

    changes made

--- test-VPC_EC2/instance.tf
+++ test-VPC_EC2/instance.tf
@@ -3,11 +3,15 @@ resource "aws_instance" "example" {
   instance_type    = var.name
 # for VPC subnet
   subnet_id        = aws_subnet.main-public-2.id
-# for Public key
-  key_name         = var.key
+# # for Public key
+#   key_name         = var.key
 # for Sg
   security_groups  = [aws_security_group.allow-ssh-login.id]
   tags = {
     Name = "elite-vm"
   }
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.deployer-key
 }
