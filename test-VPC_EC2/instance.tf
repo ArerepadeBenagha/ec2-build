184c21f083bd807ac3c24f6830e4ea683fa89416
ArerepadeB <aredelulu@gmail.com>
2021-04-24 17:09:40 -0500

    changes made

--- test-VPC_EC2/instance.tf
+++ test-VPC_EC2/instance.tf
@@ -1,17 +1,13 @@
 resource "aws_instance" "example" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
-  instance_type = "t2.micro"
-
+  ami              = lookup(var.AMIS, var.AWS_REGION)
+  instance_type    = var.name
 # for VPC subnet
-  subnet_id = aws_subnet.main-public-1.id
-
+  subnet_id        = aws_subnet.main-public-2.id
 # for Public key
-  key_name      = var.key_name
-
+  key_name         = var.key
 # for Sg
-  security_groups        = [aws_security_group.allow-ssh-login.id]
-
+  security_groups  = [aws_security_group.allow-ssh-login.id]
   tags = {
-    Name = "example-test-EC2_VPC"
+    Name = "elite-vm"
   }
 }
--- test-VPC_EC2/vars.tf
+++ test-VPC_EC2/vars.tf
@@ -10,12 +10,16 @@ variable "AMIS" {
     us-east-1 = "ami-0be2609ba883822ec"
   }
 }
-variable "public_key_path" {
-  description = "Enter the path to the SSH Public Key to add to AWS."
-  default = "/c/Users/louis/Documents/Github/Terraform-course-pratice/test-VPC_EC2"
-}
+# variable "public_key_path" {
+#   description = "Enter the path to the SSH Public Key to add to AWS."
+#   default = "/c/Users/lbena/OneDrive/Documents/DevOps Lecture/Terraform-course-pratice/test-VPC_EC2"
+# }
 
 variable "key_name" {
   description = "Key name for SSHing into EC2"
-  default = "mykey"
+  default = "/c/Users/lbena/OneDrive/Documents/DevOps Lecture/Terraform-course-pratice/test-VPC_EC2"
+}
+variable "name" {
+  description = "size of ec2"
+  default = "t2.micro"
 }
