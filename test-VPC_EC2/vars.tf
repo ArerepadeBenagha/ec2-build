3d309fcd8987c75d61ab09f81dde7024d5e9c31b
ArerepadeB <aredelulu@gmail.com>
2021-04-24 17:16:01 -0500

    changes made

--- test-VPC_EC2/vars.tf
+++ test-VPC_EC2/vars.tf
@@ -15,9 +15,9 @@ variable "AMIS" {
 #   default = "/c/Users/lbena/OneDrive/Documents/DevOps Lecture/Terraform-course-pratice/test-VPC_EC2"
 # }
 
-variable "key_name" {
+variable "deployer-key" {
   description = "Key name for SSHing into EC2"
-  default = "/c/Users/lbena/OneDrive/Documents/DevOps Lecture/Terraform-course-pratice/test-VPC_EC2"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCsGmLdQLzVmtV8UxkMlprakks7+NOTjh0P7VS2t4BId7Ipb+tqzJknxfdI/yaPR2APGPROU+f5dtwP5IXEP87BNvNhNsAZj6dpT5vzM4B5/aBUYSXk7l+0WZ+NOZsFegwo6+p5NO4aPe4aVdhaBbux3gg1011uEz6vPvm5OVyngMMER1DNcJdMG4ymL1C1GmXjKCtyWU79j8weONQugw3zMdKMWZyjU5JA37ZHO2aV6i4mKuuXQqbh/ASG/UhU4i0w6mD6cUZ7l6VozRDKs5ZtlbcZWuBPB4Dt6qKd3ADUCm552qxsI8WbOuNWfzM+BUw5ZScam26hDjfhhjlSIFG44Pal5sKHOed0+GqOT/uAUV58TEYbF5TlCPfcXintEd36ZNKxzo2K+FKlBJWKy4I2v2R/FfNVsIvipJnOmEbKG76BcHu4ZYnhlr3U70Mt/8E78VPREH+YwPlS0nQvzmdgsOuDpA+T0+Ukwf7IVaTmspkDCibX8qgAyzhxvdl4kH8= lbena@LAPTOP-QB0DU4OG"
 }
 variable "name" {
   description = "size of ec2"
