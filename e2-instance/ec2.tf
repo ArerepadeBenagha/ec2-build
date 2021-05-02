9a0806f313513af5c49fba80ed3eb47afffa8f54
ArerepadeB <aredelulu@gmail.com>
2021-05-02 10:00:34 -0500

    increase resources

--- e2-instance/ec2.tf
+++ e2-instance/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   ami           = lookup(var.amis, var.aws_region)
   instance_type = var.instance_type
   count         = var.instance_count
-  subnet_id = aws_subnet.main-public-1.id
+  subnet_id     = aws_subnet.main-public-1.id
   key_name      = aws_key_pair.mykeypair.key_name
   vpc_security_group_ids = [aws_security_group.example-instance.id]
   tags = {
--- e2-instance/vars.tf
+++ e2-instance/vars.tf
@@ -29,9 +29,101 @@ variable "key_name" {
 
 variable "instance_tags" {
   type = list
-  default = ["elite-vm-master", "elite-vm-agent"]
+  default = ["elite-vm-master", "elite-vm-agent-a", "elite-vm-agent-b"]
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t2.large"
+}
+
+variable "instance_count" {
+  type = string
+  default = "3"
+}
+
+variable "instance_list" {
+  type = list
+  default = ["i-032b6a3901529f6d0", "i-0e06f3fef9aedf43a"]
+}
+
+variable "elite-device-names" {
+  default = [
+    "/dev/xvdh",
+    "/dev/sdd",
+    "/dev/sde",
+    "/dev/sdf",
+   ]
+}
+
+variable "elite_ebs_volume_count" {
+  type = string
+  default = "1"
+}
+
+variable "elite_ebs_volume_size" {
+  default = 10
+}
+
+variable "volume_type" {
+  default = "gp2"
+}
+
+variable "db_name_subnet" {
+  description = "name of database instance"
+  default     = "mariadb-subnet"
+}
+
+variable "db_parameters" {
+  description = "for mariadb-parameters"
+  default     = "mariadb-parameters"
+}
+
+variable "family" {
+  description = "maria db family"
+  default     = "mariadb10.5"
+}
+
+variable "db_name" {
+  description = "engine database instance"
+  default     = "mariadb"
+}
+
+variable "engine_version" {
+  description = "engine version"
+  default     = "10.5"
+}
+
+variable "engine" {
+  description = "engine for mariadb"
+  default     = "mariadb"
+}
+
+variable "class" {
+  description = "maria db class"
+  default     = "db.t2.small"
+}
+
+variable "storage" {
+  description = "size of db storage"
+  default     = 100
+}
+
+variable "username" {
+  description = "username to mariadb instance"
+  default     = "root"
+}
+
+variable "RDS_PASSWORD" {
+  description = "password to mariadb instance"
+  default     = "football3$!900xvz"
+}
+
+variable "multi_az" {
+  description = "availabilty of db"
+  default     = false
+}
+
+variable "storage_type" {
+  description = "maria db size"
+  default     = "gp2"
 }
